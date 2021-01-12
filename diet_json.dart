c25781b10f117505c24e022eb8d2f8bb90a0508a
pgit97 <59139512+pgit97@users.noreply.github.com>
2021-01-12 23:02:33 +0530

    Add files via upload

---
+++ diet_json.dart
@@ -0,0 +1,58 @@
+{
+  "exercises": [
+    {
+      "id": "1",
+      "title": "Breakfast",
+      "description": "One grapefruit
+                      Two poached eggs (or fried in a non-stick pan)
+                      Two slices whole-grain toast with one pat of butter each
+                      One cup low-fat milk
+                      One cup of black coffee or herbal tea",
+      "thumbnail": "https://i1.wp.com/www.eatthis.com/wp-content/uploads/2020/04/breakfast-protein-bento-box-eggs-cottage-cheese-fruit-cucumber.jpg?fit=1200%2C879&ssl=1",
+    },
+     {
+      "id": "2",
+      "title": "Snack",
+      "description": "One banana
+                      One cup plain yogurt with two tablespoons honey
+                      Glass of water",
+      "thumbnail": "https://blog.fitbit.com/wp-content/uploads/2018/09/0910-healthy-snack-ideas-HERO.jpg",
+    },
+     {
+      "id": "3",
+      "title": "Lunch",
+      "description": "Chicken breast (6-ounce portion), baked or roasted (not breaded or fried)
+                      Large garden salad with tomato and onion with one cup croutons, topped with one tablespoon oil and vinegar (or salad dressing)
+                      Glass of water ",
+      "thumbnail": "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/quick-chicken-and-hummus-bowl-220e449.jpg?quality=90&resize=960,872",
+    },
+     {
+      "id": "4",
+      "title": "Snack",
+      "description": "One cup carrot slices
+                      Three tablespoons hummus
+                      One-half piece of pita bread
+                      Glass of water or herbal tea",
+      "thumbnail": "https://2.bp.blogspot.com/-WfJ7zv-JNdw/Uj9gRYJ6ejI/AAAAAAAACk8/FkE5bfjpH2g/s1600/Image+5.jpg",
+    },
+     {
+      "id": "5",
+      "title": "Dinner",
+      "description": "One cup steamed broccoli
+                      One cup of brown rice
+                      Halibut (four-ounce portion)
+                      Small garden salad with one cup spinach leaves, tomato, and onion topped with two tablespoons oil and vinegar or salad dressing
+                      One glass white wine (regular or dealcoholized)
+                      Sparkling water with lemon or lime slice",
+      "thumbnail": "https://i.ytimg.com/vi/EdFQjmrJuhg/maxresdefault.jpg",
+    },
+     {
+      "id": "6",
+      "title": "Snacks",
+      "description": "One cup blueberries
+                      Two tablespoons whipped cream (the real stuff—whip your own or buy in a can)
+                      Glass of water",
+      "thumbnail": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/two-rows-of-various-garnished-sandwiches-royalty-free-image-1588774545.jpg",
+    },
+  ]
+}
